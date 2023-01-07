1fcb94e3237925aec6f9628db6a8579a25c26f9a
lumanexx <techlumanex@gmail.com>
2023-01-07 03:45:31 -0800

    11 pow

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    if i == 0:
-        i = 32
-    else:
-        i = 0
+def pow(a, b): return a ** 
