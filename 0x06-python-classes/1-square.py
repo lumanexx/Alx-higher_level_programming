7143d9b09779064243653172cae36d709c81f3e4
lumanexx <techlumanex@gmail.com>
2023-01-24 14:19:03 -0800

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""define a class square"""
+
+
+class Square:
+    """A square"""
+
+    def __init__(self, size):
+        """initialize a new square
+        Args:
+            size (int): size of square
+        """
+
+        self.__size = size
